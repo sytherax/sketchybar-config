ec5a425c9f1f2a183a12d17441a96662912414d5
Kcraft059 <kcraft059.msg@gmail.com>
2025-08-26 09:43:42 +0200

    Minor changes for a more subtle styling

--- install.sh
+++ install.sh
@@ -3,15 +3,21 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Colors
-GREEN="\033[0;32m"
-YELLOW="\033[1;33m"
-RED="\033[0;31m"
+
+spmt='\033[38;5;5;1m> \033[0m\033[48;5;0m' # >_
+sqes='\033[48;5;0;38;5;8m[\033[38;5;4m?\033[38;5;8m]\033[0m' # [?]
+scac='\033[48;5;0;38;5;8m[\033[38;5;1mx\033[38;5;8m]\033[0m' # [x]
+sexc='\033[48;5;0;38;5;8m[\033[38;5;2mo\033[38;5;8m]\033[0m' # [o]
+smak='\033[48;5;0;38;5;8m[\033[38;5;5m+\033[38;5;8m]\033[0m' # [+]
+swrn='\033[48;5;0;38;5;8m[\033[38;5;3m!\033[38;5;8m]\033[0m' # [!]
+syon='(\033[38;5;2;1my\033[38;5;0m/\033[38;5;1;1mn\033[0m)' # (y/n)
+
 RESET="\033[0m"
 
 # Logging helpers
-log()     { echo -e "${YELLOW}➤ $1${RESET}"; }
-success() { echo -e "${GREEN}✔ $1${RESET}"; }
-error()   { echo -e "${RED}✖ $1${RESET}" >&2; exit 1; }
+log()     { echo -e "${sexc} $1${RESET}"; }
+success() { echo -e "${smak} $1${RESET}"; }
+error()   { echo -e "${scac} $1${RESET}" >&2; exit 1; }
 
 # Ensure dependencies
 for cmd in git brew curl jq; do
@@ -50,10 +56,10 @@ curl -fsSL -o "$output_path" "$font_url" || error "Failed to download dyn-icon_m
 success "Downloaded dyn-icon_map.sh → $output_path"
 
 ### GitHub Notifications Setup
-read -rp "$(echo -e "${YELLOW}➤ Do you want to enable GitHub notifications in SketchyBar? (y/n): ${RESET}")" enable_github
+read -rp "$(echo -e "${sqes} Do you want to enable GitHub notifications in SketchyBar? ${syon}: ${RESET}")" enable_github
 
 if [[ "$enable_github" =~ ^[Yy]$ ]]; then
-  read -rsp "$(echo -e "${YELLOW}➤ Please enter your Classic GitHub Token: ${RESET}")" github_token
+  read -rsp "$(echo -e "${sqes} Please enter your Classic GitHub Token: ${RESET}")" github_token
   echo
   if [[ -n "$github_token" ]]; then
     echo "$github_token" > "$HOME/.github_token"
