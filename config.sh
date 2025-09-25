f560530f48c8673a1938ee37832d47b6689c4aa8
Kcraft059 <kcraft059.msg@gmail.com>
2025-09-25 19:13:47 +0200

    Made all config values as exmaples

--- config.sh
+++ config.sh
@@ -4,14 +4,14 @@
 
 # Width (in px) of the display notch reserved space
 # Increase if music or other items are clipped near the notch
-NOTCH_WIDTH=180
+#NOTCH_WIDTH=180
 
 # Music title/subtitle info width (px)
-MUSIC_INFO_WIDTH=80
+#MUSIC_INFO_WIDTH=80
 
 # CPU graph update frequency (seconds)
-CPU_UPDATE_FREQ=2
+#CPU_UPDATE_FREQ=2
 
 # Theme
-COLOR_SCHEME="catppuccin-mocha"
+#COLOR_SCHEME="rosepine-moon"
 #THEME_FILE_PATH="$HOME/.config/sketchybar-config/theme.sh"
