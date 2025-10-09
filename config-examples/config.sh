5130bff97c1dd8306a347b5d39de427e2b70bc25
sytherax <swooruptrader@gmail.com>
2025-10-10 01:27:11 +1100

    Add Aerospace window manager support and related scripts
    - Introduced Aerospace integration in README.md with setup instructions.
    - Added aerospace-script.sh for workspace management.
    - Created aerospace-windows.sh to update workspace labels with app icons.
    - Updated spaces.sh to handle workspace events for Aerospace mode.
    - Configured sketchybar to trigger events based on Aerospace mode.
    - Set default AEROSPACE_MODE in sketchybarrc.

--- config-examples/config.sh
+++ config-examples/config.sh
@@ -32,3 +32,6 @@ WIFI_UNREDACTOR="~/Applications/wifi-unredactor.app"
 COLOR_SCHEME="rosepine-moon"
 BAR_TRANSPARENCY=false
 THEME_FILE_PATH="$HOME/.config/sketchybar-config/theme.sh"
+
+# Aerospace window manager support
+AEROSPACE_MODE=False
