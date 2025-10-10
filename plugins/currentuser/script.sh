103ddeb8fb4983766bb04dc8fab331ccb54772c1
sytherax <93241048+sytherax@users.noreply.github.com>
2025-10-10 23:09:52 +1100

    Fix RealName
    Was getting `@RealName:` instead of showing the actual name. As my name was on a new line after the header.

--- plugins/currentuser/script.sh
+++ plugins/currentuser/script.sh
@@ -2,5 +2,5 @@
 
 ### Get real name of current user
 
-RealName=$(dscl . -read /Users/$(whoami) | awk '/RealName/ {gsub("RealName: ",""); print}')
+RealName=$(dscl . -read /Users/"$(whoami)" RealName | awk 'NR>1 {sub(/^[[:space:]]*/, ""); print; exit}')
 sketchybar --set "$NAME" label="$RealName"
